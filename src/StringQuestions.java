11d382c6f3b35e244896659dbd1834f10415f1c1
rey-rubio <reynerio.r.rubio@gmail.com>
2019-03-22 00:03:38 -0400

    StringQuestions.java - longestPalindrome - numIslands - lengthOfLongestSubstring

--- src/StringQuestions.java
+++ src/StringQuestions.java
@@ -20,6 +20,37 @@ public class StringQuestions {
 
     }
 
+    public String longestPalindrome(String s){
+        int length = s.length();
+        if(s == null || length < 1)
+            return "";
+        int begin = 0;
+        int end = 0;
+
+        for(int i = 0; i < length; i++){
+            int length1 = palindromeHelper(s, i, i);
+            int length2 = palindromeHelper(s, i, i+1);
+            int maxLength = Math.max(length1, length2);
+            if(maxLength > end - begin){
+                begin = i - (maxLength - 1) / 2;
+                end = i + (maxLength / 2);
+            }
+
+        }
+
+        return s.substring(begin, end + 1);
+    }
+
+    private int palindromeHelper(String s, int l, int r){
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
+        }
+        return r - l - 1;
+
+    }
+
+
     public boolean wordPattern(String pattern, String str) {
 
         String[] strArr = str.split(" ");
@@ -159,6 +190,63 @@ public class StringQuestions {
         return true;
     }
 
+
+    private int numRows;
+    private int numCols;
+    public int numIslands(char[][] grid){
+
+        numRows = grid.length;
+        if(numRows == 0)
+            return 0;
+
+        numCols = grid[0].length;
+        int count = 0;
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numCols; j++){
+                if(grid[i][j] == '1'){
+                    DFSIslandHelper(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void DFSIslandHelper(char[][]grid, int i, int j){
+        if(i < 0 || j < 0 || i >= numRows || j >= numCols || grid[i][j] != '1')
+            return;
+        // Unmark this and other surrounding 1s (Up, Down, Left, Right)
+        grid[i][j] = 0;
+        DFSIslandHelper(grid, i-1, j);
+        DFSIslandHelper(grid, i+1, j);
+        DFSIslandHelper(grid, i, j-1);
+        DFSIslandHelper(grid, i, j+1);
+    }
+
+
+    public int lengthOfLongestSubstring(String s){
+        int len = s.length();
+        int answer = 0;
+
+        if(len <= 1)
+            return len;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0, j = 0; j < len; j++){
+            // if HashMap contains the character at this j index,
+            //   move left index to position of character + 1
+            if(map.containsKey(s.charAt(j)))
+                i = Math.max(map.get(s.charAt(j)) + 1, i);
+
+            answer = Math.max(answer, j - i  + 1);
+            map.put(s.charAt(j), j);
+        }
+        return answer;
+
+
+    }
+
     public static void main(String[] args)
     {
         StringQuestions test = new StringQuestions();
@@ -211,5 +299,34 @@ public class StringQuestions {
         String S = "aAAbbbbtsdfdrtsdfgsdrtsdfg";
         System.out.println(String.format("J = %s, S = %s", J, S));
         System.out.println(test.numJewelsInStones(J, S));
+
+
+        System.out.println("__________________________________________________________________\n");
+        System.out.println("public int numIslands(char[][] grid)");
+        char[][] grid = {   {'1', '1', '1', '1', '0'},
+                            {'1', '1', '0', '1', '0'},
+                            {'1', '1', '0', '0', '0'},
+                            {'0', '0', '1', '0', '1'}};
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Number of Islands: " + test.numIslands(grid));
+
+
+
+        System.out.println("__________________________________________________________________\n");
+        System.out.println("public String longestPalindrome(String s)");
+        String s = "babad";
+        System.out.println(String.format("Longest Palindrome in \"%s\" is \"%s\"", s, test.longestPalindrome(s)));
+
+
+        System.out.println("__________________________________________________________________\n");
+        System.out.println("public int lengthOfLongestSubstring(String s)");
+        s = "abcabcbb";
+        System.out.println(String.format("Length of Longest Substring without Repeating Characters in \"%s\" is %d", s, test.lengthOfLongestSubstring(s)));
     }
 }
